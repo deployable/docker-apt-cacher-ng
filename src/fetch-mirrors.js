40cdb96edf04e2e5d7179b085b5daa840f3ad017
Matt Hoyle <code@deployable.co>
2018-01-26 04:30:00 +0000

    Babel compile the script so it's all bluebird long stack promises

--- src/fetch-mirrors.js
+++ src/fetch-mirrors.js
@@ -1,139 +1,174 @@
-const Promise = require('bluebird')
-const cheerio = require('cheerio')
-const needle = require('needle')
-const debug = require('debug')('mh:docker:kind:mirrors')
-const fs = Promise.promisifyAll(require('fs'))
-const readline = require('readline')
-const path = require('path')
+'use strict';
 
-async function goNet(){
-  let response = await needle('get', 'https://www.centos.org/download/mirrors/')
-  let $ = cheerio.load(response.body)
-  debug(response.body)
-  let mirrors = $('table.CSVTable tr').map((row,el) => $(el).children('td').slice(3,4).text()).get()
-  console.log(mirrors.join('\n'))
-}
+var _bluebird = require('bluebird');
 
-async function fetchCentos(){
-  return new Promise((resolve,reject) => {
-    let response = needle.get('https://www.centos.org/download/full-mirrorlist.csv')
-    const line_reader = require('readline').createInterface({
-      input: response
-    })
+const Promise = require('bluebird');
+Promise.config({ longStackTraces: true, warnings: true });
+const cheerio = require('cheerio');
+const needle = require('needle');
+const debug = require('debug')('mh:docker:kind:mirrors');
+const fs = Promise.promisifyAll(require('fs'));
+const readline = require('readline');
+const path = require('path');
 
-    const mirrors = []
+class Fetch {
 
-    line_reader.on('line', line => {
-      line.replace(/^"/,'').replace(/"$/,'')
-      let fields = line.split(/","/)
-      if ( fields.length > 4 && fields[4] ) {
-        //debug('fields', fields)
-        mirrors.push(fields[4])
-      } 
-      else {
-        console.error('bad line', line)
-      }
-    })
-
-    line_reader.on('close', ()=>{
-      debug('centos mirrors', mirrors.join("\n"))
-      resolve(mirrors)
-    })
-
-    line_reader.on('error', ()=> reject(error))
-  })
-}
-
-async function fetchEpel(){
-  let response = await needle('get', 'https://admin.fedoraproject.org/mirrormanager/mirrors/EPEL')
-  let $ = cheerio.load(response.body)
-  let $rows = $('.container table').first().find('tr')
-  let mirrors = []
-  $rows.each((row,el) => {
-    let mirror_info = $(el).children('td').slice(3,4).contents()
-    let mode = null
-    mirror_info.each((mirror_data, el) => {
-      if ( el.type === 'text' ) {
-        if ( /Fedora EPEL/.exec(el.data) ) mode = 'epel'
-        if ( /Fedora Linux/.exec(el.data) ) mode = 'fedora'
-      }
-      if ( mode === 'epel' && el.type === 'tag' && el.name === 'a' && $(el).text() === 'http' ) {
-        mirrors.push($(el).attr('href'))
-      }
-    })
-  })
-  debug('epel mirrors', mirrors.join('\n'))
-  return mirrors 
-}
-
-async function fetchFedora(){
-  let response = await needle('get', 'https://admin.fedoraproject.org/mirrormanager/mirrors/Fedora')
-  let $ = cheerio.load(response.body)
-  let $rows = $('.container table').first().find('tr')
-  let mirrors = []
-  /*
-  $rows.map((row,el) => {
-    return $(el).children('td').slice(3,4).find('a').each((linki, el)=> {
-      if ( $(el).text() === 'http' ) mirrors.push($(el).attr('href'))
-    })
-  })
-  */
-  $rows.each((row,el) => {
-    let mirror_info = $(el).children('td').slice(3,4).contents()
-    let mode = null
-    mirror_info.each((mirror_data, el) => {
-      if ( el.type === 'text' ) {
-        if ( /Fedora EPEL/.exec(el.data) ) mode = 'epel'
-        if ( /Fedora Linux/.exec(el.data) ) mode = 'fedora'
-      } 
-      if ( mode === 'fedora' && el.type === 'tag' && el.name === 'a' && $(el).text() === 'http' ) {
-        mirrors.push($(el).attr('href'))  
-      }
-    })
-  })
-  debug('fedora mirrors', mirrors.join('\n'))
-  return mirrors 
-}
-
-async function fetchApache(){
-  let response = await needle('get', 'https://www.apache.org/mirrors/dist.html')
-  let $ = cheerio.load(response.body)
-  let $rows = $('table tr')
-  mirrors = []
-  $rows.each((rowi, el) => {
-    let $el = $(el)
-    let cols = $el.find('td')
-    debug('apache row %s: size %s: ', rowi, cols.length, $(cols).text())
-    if ( cols.length === 5 ) {
-      let $mirror_col = $( $(cols).get(0) )
-      let $scheme_col = $( $(cols).get(1) )
-      let is_http = $scheme_col.text().includes('http')
-      let mirror_link = $mirror_col.find('a').first().attr('href')
-      debug('is_https: %s  mirror: %s', is_http, mirror_link)
-      if ( is_http === true ) mirrors.push(mirror_link)
-    }
-  })
-  return mirrors
-}
-
-async function writeMirror( file, pmData ){
-  let file_path = path.resolve( __dirname, '..', 'files', file )
-  if ( typeof pmData === 'function') pmData = pmData()
-  let mirror_data = await pmData
-  return fs.writeFileAsync(file_path, mirror_data.join('\n'))
-}
-
-async function go(){
-  try {
-    //writeMirror('centos_mirrors', fetchCentos())
-    //writeMirror('fedora_mirrors', fetchFedora())
-    //writeMirror('epel_mirrors', fetchEpel())
-    writeMirror('apache_mirrors', fetchApache)
-  }
-  catch (error) {
-    console.log(error)
+  static fetchAndCheerio(url) {
+    return (0, _bluebird.coroutine)(function* () {
+      debug('retriving url: %s', url);
+      let response = yield needle('get', url);
+      debug('retrieved response:', response.body);
+      return cheerio.load(response.body);
+    })();
   }
 
+  static /*async*/fetchCentos() {
+    return new Promise((resolve, reject) => {
+      let response = needle.get('https://www.centos.org/download/full-mirrorlist.csv');
+      const line_reader = require('readline').createInterface({
+        input: response
+      });
+
+      const mirrors = [];
+
+      line_reader.on('line', line => {
+        // poor mans csv parsing, remove leading and trailing `"`s
+        // then splut the string on `","`. 
+        // Only works when every field is quoted with `"`
+        line.replace(/^"/, '').replace(/"$/, '');
+        let fields = line.split(/","/);
+        if (fields.length > 4 && fields[4]) {
+          //debug('fields', fields)
+          mirrors.push(fields[4]);
+        } else {
+          if (fields.length === 1 || fields[5] && fields[5].includes('ftp')) return;
+          console.error('centos - bad line', line);
+        }
+      });
+
+      line_reader.on('close', () => {
+        debug('centos mirrors', mirrors.join("\n"));
+        resolve(mirrors);
+      });
+
+      line_reader.on('error', () => reject(error));
+    });
+  }
+
+  static fetchEpel() {
+    var _this = this;
+
+    return (0, _bluebird.coroutine)(function* () {
+      let $ = yield _this.fetchAndCheerio('https://admin.fedoraproject.org/mirrormanager/mirrors/EPEL');
+      let $rows = $('.container table').first().find('tr');
+      let mirrors = [];
+      $rows.each(function (row, el) {
+        let mirror_info = $(el).children('td').slice(3, 4).contents();
+        let mode = null;
+        mirror_info.each(function (mirror_data, el) {
+          if (el.type === 'text') {
+            if (/Fedora EPEL/.exec(el.data)) mode = 'epel';
+            if (/Fedora Linux/.exec(el.data)) mode = 'fedora';
+          }
+          if (mode === 'epel' && el.type === 'tag' && el.name === 'a' && $(el).text() === 'http') {
+            mirrors.push($(el).attr('href'));
+          }
+        });
+      });
+      debug('epel mirrors', mirrors.join('\n'));
+      return mirrors;
+    })();
+  }
+
+  static fetchFedora() {
+    var _this2 = this;
+
+    return (0, _bluebird.coroutine)(function* () {
+      let $ = yield _this2.fetchAndCheerio('https://admin.fedoraproject.org/mirrormanager/mirrors/Fedora');
+      let $rows = $('.container table').first().find('tr');
+      let mirrors = [];
+      /*
+      $rows.map((row,el) => {
+        return $(el).children('td').slice(3,4).find('a').each((linki, el)=> {
+          if ( $(el).text() === 'http' ) mirrors.push($(el).attr('href'))
+        })
+      })
+      */
+      $rows.each(function (row, el) {
+        let mirror_info = $(el).children('td').slice(3, 4).contents();
+        let mode = null;
+        mirror_info.each(function (mirror_data, el) {
+          if (el.type === 'text') {
+            if (/Fedora EPEL/.exec(el.data)) mode = 'epel';
+            if (/Fedora Linux/.exec(el.data)) mode = 'fedora';
+          }
+          if (mode === 'fedora' && el.type === 'tag' && el.name === 'a' && $(el).text() === 'http') {
+            mirrors.push($(el).attr('href'));
+          }
+        });
+      });
+      debug('fedora mirrors', mirrors.join('\n'));
+      return mirrors;
+    })();
+  }
+
+  static fetchApache() {
+    var _this3 = this;
+
+    return (0, _bluebird.coroutine)(function* () {
+      let $ = yield _this3.fetchAndCheerio('https://www.apache.org/mirrors/dist.html');
+      let $rows = $('table tr');
+      let mirrors = [];
+      $rows.each(function (rowi, el) {
+        let $el = $(el);
+        let cols = $el.find('td');
+        debug('apache row %s: size %s: ', rowi, cols.length, $(cols).text());
+        if (cols.length === 5) {
+          let $mirror_col = $($(cols).get(0));
+          let $scheme_col = $($(cols).get(1));
+          let is_http = $scheme_col.text().includes('http');
+          let mirror_link = $mirror_col.find('a').first().attr('href');
+          debug('is_https: %s  mirror: %s', is_http, mirror_link);
+          if (is_http === true) mirrors.push(mirror_link);
+        }
+      });
+      return mirrors;
+    })();
+  }
+
+  static writeMirror(file, promise) {
+    return (0, _bluebird.coroutine)(function* () {
+      let file_path = path.resolve(__dirname, '..', 'files', file);
+      if (typeof promise === 'function') promise = promise();
+      let mirror_data = yield promise;
+      debug('writeMirror has got the mirror data for file "%s"', file);
+      return fs.writeFileAsync(file_path, mirror_data.join('\n'));
+    })();
+  }
+
+  static go() {
+    var _this4 = this;
+
+    return (0, _bluebird.coroutine)(function* () {
+      try {
+        yield _this4.writeMirror('centos_mirrors', function () {
+          return _this4.fetchCentos();
+        });
+        yield _this4.writeMirror('fedora_mirrors', function () {
+          return _this4.fetchFedora();
+        });
+        yield _this4.writeMirror('epel_mirrors', function () {
+          return _this4.fetchEpel();
+        });
+        yield _this4.writeMirror('apache_mirrors', function () {
+          return _this4.fetchApache();
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }
 }
 
-go()
+Fetch.go();
+
